52809cb7bd06ebd58f9a10af79c73700904ed501
igor vicente <igor_machado05@hotmail.com>
2024-04-21 21:42:24 -0300

    getting values from input with useRef

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-export default function Login() {
+export default function LoginState() {
   const [enteredValues, setenteredValues] = useState({
     email: '',
     password: '',
