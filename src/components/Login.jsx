aebcac0aceeb7ce1a740166b6025b7cbbed14dc5
igor vicente <igor_machado05@hotmail.com>
2024-04-21 21:16:48 -0300

    getting values from input

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,23 +1,53 @@
+import { useState } from 'react';
+
 export default function Login() {
+  const [enteredValues, setenteredValues] = useState({
+    email: '',
+    password: '',
+  });
+
+  function handleInputChange(identifier, value) {
+    setenteredValues((prevState) => ({ ...prevState, [identifier]: value }));
+  }
+  console.log('RENDERED!');
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(enteredValues);
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            value={enteredValues.email}
+            onChange={(evt) => handleInputChange('email', evt.target.value)}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            value={enteredValues.password}
+            onChange={(evt) => handleInputChange('password', evt.target.value)}
+          />
         </div>
       </div>
 
       <p className="form-actions">
         <button className="button button-flat">Reset</button>
-        <button className="button">Login</button>
+        <button className="button" onClick={handleSubmit}>
+          Login
+        </button>
       </p>
     </form>
   );
